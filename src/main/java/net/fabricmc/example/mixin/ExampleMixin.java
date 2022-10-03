c5e95b4c6a6f345c8fa6275ea0603d79913a32f7
Lucy <gakupo.gumi@gmail.com>
2022-10-03 20:44:02 +1300

    Init

---
+++ src/main/java/net/fabricmc/example/mixin/ExampleMixin.java
@@ -0,0 +1,12 @@
+package net.fabricmc.example.mixin;
+
+import net.fabricmc.example.Main;
+import net.minecraft.client.gui.screen.TitleScreen;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(TitleScreen.class)
+public class ExampleMixin {
+}
